1a38f56f1887f4e8df1aa0af8a7f6848ec2271ff
Jędrzej <39235661+LuckyMyLove@users.noreply.github.com>
2022-01-17 20:13:47 +0100

    Resize script updated

--- risize_pictures.py
+++ risize_pictures.py
@@ -2,15 +2,19 @@
 from PIL import Image
 import os, sys
 
-path = "./data/resize_test"
+path = "./data/resize_test/" #Change path to the one where pictures are
 dirs = os.listdir( path )
 
 def resize():
     for item in dirs:
+        print(os.path.dirname(item))
+
         if os.path.isfile(path+item):
             im = Image.open(path+item)
             f, e = os.path.splitext(path+item)
-            imResize = im.resize((128,128), Image.ANTIALIAS)
-            imResize.save(f + ' resized.jpg', 'JPEG', quality=90)
+            imResize = im.resize((256,256), Image.ANTIALIAS)
+            imResize.save(f + '.jpg', 'JPEG', quality=90)
+
+    print("Resize done")
 
 resize()
