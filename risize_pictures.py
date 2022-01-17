7afe4165779628cc2ab8f5fcebd5f3eb064aede8
Jędrzej <39235661+LuckyMyLove@users.noreply.github.com>
2022-01-17 21:43:43 +0100

    Final version of the script

--- risize_pictures.py
+++ risize_pictures.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python
 from PIL import Image
 import os, sys
+from pathlib import Path
 
-path = "./data/resize_test/" #Change path to the one where pictures are
-dirs = os.listdir( path )
+path_test = "./data/test/" #Change path to the one where pictures are
+path_train = "./data/train/"
 
-def resize():
+mashroom_kinds = os.listdir( path_test )
+
+def resize(kind, type):
+    if type == "test":
+        path = path_test + kind + "/"
+        destination_path = './data/resized_pictures/test/' + kind + '/'
+    else:
+        path = path_train + kind + "/"
+        destination_path = './data/resized_pictures/train/' + kind + '/'
+
+    dirs = os.listdir( path )
+
     for item in dirs:
-        print(os.path.dirname(item))
-
         if os.path.isfile(path+item):
             im = Image.open(path+item)
-            f, e = os.path.splitext(path+item)
+            file_name = item.split('.')[0]
+
+            Path(destination_path).mkdir(parents=True, exist_ok=True)
+
             imResize = im.resize((256,256), Image.ANTIALIAS)
-            imResize.save(f + '.jpg', 'JPEG', quality=90)
+            imResize.save(destination_path + file_name + '.jpg', 'JPEG', quality=90)
 
-    print("Resize done")
 
-resize()
+
+for kind in mashroom_kinds:
+    resize(kind, 'test')
+    resize(kind, 'train')
+
+
+print("Resize done")
